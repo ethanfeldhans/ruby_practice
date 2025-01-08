85ff6bc0dbd235928e2a4dc4121cf08c8b7c72d3
ethanfeldhans <ethan.feldhans@gmail.com>
2025-01-07 19:19:22 -0600

    add store_item.rb

---
+++ store_item.rb
@@ -0,0 +1,10 @@
+# C.1 - You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
+
+# C.2 - Represent 3 items using hashes. Each hash should have the same keys with different values.
+
+# C.3 - Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
+shirt1 = {"brand" => "Nike", "size" => "Medium", "color" => "red"}
+shirt2 = {"brand" => "Adidas", "size" => "Small", "color" => "green"}
+shirt3 = {"brand" => "Under Armour", "size" => "Large", "color" => "blue"}
+
+pp shirt1, shirt2, shirt3
