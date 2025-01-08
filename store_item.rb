60fb82c52761a83595a727d7de47cac85f8bc0fb
ethanfeldhans <ethan.feldhans@gmail.com>
2025-01-07 20:43:06 -0600

    add store_item

--- store_item.rb
+++ store_item.rb
@@ -3,8 +3,24 @@
 # C.2 - Represent 3 items using hashes. Each hash should have the same keys with different values.
 
 # C.3 - Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
-shirt1 = {"brand" => "Nike", "size" => "Medium", "color" => "red"}
-shirt2 = {"brand" => "Adidas", "size" => "Small", "color" => "green"}
-shirt3 = {"brand" => "Under Armour", "size" => "Large", "color" => "blue"}
+# shirt1 = {"brand" => "Nike", "size" => "Medium", "color" => "red"}
+# shirt2 = {"brand" => "Adidas", "size" => "Small", "color" => "green"}
+# shirt3 = {"brand" => "Under Armour", "size" => "Large", "color" => "blue"}
 
-pp shirt1, shirt2, shirt3
+# pp shirt1, shirt2, shirt3
+
+
+class Shirt
+  def initialize(input_brand, input_size, input_color)
+    @brand = input_brand
+    @size = input_size
+    @color = input_color
+  end
+
+  def print_info
+    p "#{@brand} #{@input} #{@color}"
+  end
+
+end
+
+p Shirt.new("Nike", "Medium", "Red")
